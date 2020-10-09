901ba9bfb773a135c3dc2080dfab6ee22610fd72
fatihis <isk.fatih1@gmail.com>
2020-10-09 03:55:28 +0300

    build react app

---
+++ VaccineTracker/VaccineTracker/ClientApp/src/components/FetchData.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+export class FetchData extends Component {
+  static displayName = FetchData.name;
+
+  constructor(props) {
+    super(props);
+    this.state = { forecasts: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.populateWeatherData();
+  }
+
+  static renderForecastsTable(forecasts) {
+    return (
+      <table className='table table-striped' aria-labelledby="tabelLabel">
+        <thead>
+                <tr>
+
+                    <th>ID</th>
+                    <th>Last Vaccine Date</th>
+                    <th>Age</th>
+                    <th>Next Appointment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map(forecast =>
+              <tr key={forecast.temperatureC}>
+                  <td>{forecast.temperatureC}</td>
+                  <td>{forecast.date}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : FetchData.renderForecastsTable(this.state.forecasts);
+
+    return (
+      <div>
+        <h1 id="tabelLabel" >Pet List</h1>
+        <p>In progress</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async populateWeatherData() {
+    const response = await fetch('weatherforecast');
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  }
+}
---
+++ VaccineTracker/VaccineTracker/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+        <div>
+            <p>//Search module should be hear within 12 hours</p>
+
+      </div>
+    );
+  }
+}
