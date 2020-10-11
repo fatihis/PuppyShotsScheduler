0c4ad048e970fbf7d6ac254c5c3d46a937846e9a
fatihis <isk.fatih1@gmail.com>
2020-10-11 23:32:44 +0300

    setup mysql connection

--- VaccineTracker/VaccineTracker/Startup.cs
+++ VaccineTracker/VaccineTracker/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySqlConnector;
 
 namespace VaccineTracker
 {
@@ -21,7 +22,9 @@ namespace VaccineTracker
         public void ConfigureServices(IServiceCollection services)
         {
 
+
             services.AddControllersWithViews();
+            services.Add(new ServiceDescriptor(typeof(DataTool), new DataTool(Configuration.GetConnectionString("DefaultConnection"))));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
